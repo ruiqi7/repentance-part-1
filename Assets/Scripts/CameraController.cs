a05cdf55c460c174d2101232daed96af14aa906c
vk-comp <81334448+vk-comp@users.noreply.github.com>
2023-08-22 17:43:55 +1000

    Initial Player movement

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float sens = 100f;
+    float xRotation = 0f;
+    public Transform player;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    void Update()
+    {
+        // Mouse Input
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * sens;
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * sens;
+
+        xRotation -= mouseY;
+        xRotation = Math.Clamp(xRotation, -90f, 90f);
+        
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        player.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 1f;
+    public CharacterController player;
+    void Update()
+    {
+        // Keyboard Input
+        float xMove = Input.GetAxis("Horizontal");
+        float zMove = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * xMove * speed + transform.forward * zMove;
+        player.Move(move * speed * Time.deltaTime);
+    }
+}
