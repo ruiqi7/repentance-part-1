8c3ada2599eca80ce34f1e99db00827c4294a4b1
JessPollard <jlpollard@student.unimelb.edu.au>
2023-08-23 22:11:42 +1000

    random walking

---
+++ Assets/Scripts/zombieWalk.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+
+public class ZombieWalk : MonoBehaviour
+{
+    [SerializeField] private float minX, maxX;
+    [SerializeField] private float minZ, maxZ;
+    [SerializeField] private float speed;
+
+    private Vector3 targetPosition;
+
+    private Vector3 GetRandomTarget() {
+        return new Vector3(Random.Range(minX, maxX), 0f, Random.Range(minZ,maxZ));
+    }
+
+    void Start() {
+        targetPosition = GetRandomTarget();
+        transform.LookAt(targetPosition);
+        StartCoroutine(NextLoc());
+    }
+
+    IEnumerator NextLoc() {
+        yield return new WaitForSeconds(3);
+        targetPosition = GetRandomTarget();
+        transform.LookAt(targetPosition);
+        StartCoroutine(NextLoc());
+    }
+    private void Update() {
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, 0.002f);
+    }
+}
