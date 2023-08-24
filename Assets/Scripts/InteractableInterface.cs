a4354b2299c23afa88e043d09ec337965099da57
vk <81334448+vk-comp@users.noreply.github.com>
2023-08-24 17:06:20 +1000

    Interact Update

--- Assets/Scripts/InteractableInterface.cs
+++ Assets/Scripts/InteractableInterface.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public abstract class InteractableInterface : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public string interactText;
+
+    public virtual void interact() { 
+    
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Assets/Scripts/PlayerScripts/RaycastController.cs
+++ Assets/Scripts/PlayerScripts/RaycastController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InteractionController : MonoBehaviour
+public class RaycastController : MonoBehaviour
 {
     // Start is called before the first frame update
     public int interactDistance = 2;
@@ -12,9 +12,8 @@ public class InteractionController : MonoBehaviour
     }
 
     // Update is called once per 
-    void FixedUpdate()
+    void Update()
     {
-        
         // Creates a Ray from the center of the viewport
         Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
         Debug.DrawRay(ray.origin, ray.direction * 10);
@@ -22,11 +21,12 @@ public class InteractionController : MonoBehaviour
         if (Physics.Raycast(ray, out hit, interactDistance)) {
             if (hit.collider.CompareTag("Interactable")) { 
                 GameObject interact = hit.transform.gameObject;
-                interact.transform.position += Vector3.forward * 1f;
+                //interact.transform.position += Vector3.forward * 1f;
+                if (Input.GetKeyDown(KeyCode.E)) {
+                    interact.transform.position += Vector3.forward * 1f;
+                }
                 Debug.Log("Object hit");
             }
-
-
         }
     }
 }
