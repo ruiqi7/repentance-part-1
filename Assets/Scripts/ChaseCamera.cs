6661013fbc05ea29e37ef940f53f52d8e8b35130
JessPollard <jlpollard@student.unimelb.edu.au>
2023-08-25 20:52:19 +1000

    fixed raytracing

--- Assets/Scripts/ChaseCamera.cs
+++ Assets/Scripts/ChaseCamera.cs
@@ -6,31 +6,33 @@ public class ChaseCamera : MonoBehaviour
 {
     [SerializeField] private GameObject target;
     [SerializeField] private float speed;
+    [SerializeField] private float minX, maxX;
+    [SerializeField] private float minZ, maxZ;
     private Rigidbody rb;
+    private Vector3 targetPosition;
     void Start()
     {
         transform.LookAt(target.transform.position);
         rb = GetComponent<Rigidbody>();
     }
 
+    private Vector3 GetRandomTarget() {
+        return new Vector3(Random.Range(minX, maxX), 0f, Random.Range(minZ,maxZ));
+    }
+
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         RaycastHit hit;
         Vector3 direction = Camera.main.transform.position - transform.position;
-        Debug.DrawRay(transform.position, direction);
+        Debug.DrawRay(transform.position, direction, Color.yellow);
         if(Physics.Raycast(transform.position, direction, out hit)) {
-            if(hit.collider.CompareTag("MainCamera")) {
-                Debug.Log("hello");
+            if(hit.collider.tag == "Wall") {
+            } else if(hit.collider.tag == "MainCamera") {
                 Vector3 newPos = Vector3.MoveTowards(transform.position, target.transform.position, speed);
                 rb.MovePosition(new Vector3(newPos.x, 0, newPos.z));
                 transform.LookAt(new Vector3(target.transform.position.x, 0, target.transform.position.z));
             }
         }
-        // if(Vector3.Distance(target.transform.position, transform.position) < 5) {
-        //     Vector3 newPos = Vector3.MoveTowards(transform.position, target.transform.position, speed);
-        //     rb.MovePosition(new Vector3(newPos.x, 0, newPos.z));
-        //     transform.LookAt(new Vector3(target.transform.position.x, 0, target.transform.position.z));
-        // }
     }
 }
