5366f874b0dbe0d93ded7f273086673002c4b4e8
vk <81334448+vk-comp@users.noreply.github.com>
2023-08-25 16:17:42 +1000

    Asset additions

--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -9,7 +9,13 @@ public class PlayerController : MonoBehaviour
     public float sprintSpeed = 10.0f;
     public float baseSpeed = 5.0f;
     public CharacterController player;
-    void Update()
+    public LayerMask groundMask;
+
+    private bool isGrounded;
+    public Vector3 velocity;
+    
+
+    void FixedUpdate()
     {
         // Keyboard Input
         float xMove = Input.GetAxisRaw("Horizontal");
@@ -21,6 +27,14 @@ public class PlayerController : MonoBehaviour
             speed = sprintSpeed;
         } else {
             speed = baseSpeed;
+        }
+        isGrounded = Physics.Raycast(player.transform.position, Vector3.down, (player.height / 2) + 0.1f, groundMask);
+        if (isGrounded && velocity.y < 0) {
+            velocity.y = 0f;
+        }
+        if (!isGrounded) {
+            velocity.y += -9.8f;
+            player.Move(velocity * Time.deltaTime);
         }
     }
 }
