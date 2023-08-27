85afd79de888399ce6a8aaa0d357c1e39e37d645
vk-comp <vkhuat@student.unimelb.edu.au>
2023-08-28 02:09:47 +1000

    Start Scene Building + Stamina Mechanic Added

--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -1,18 +1,22 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Analytics;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed;
     public float sprintSpeed = 10.0f;
     public float baseSpeed = 5.0f;
+    public float stamina = 100;
+    
     public CharacterController player;
     public LayerMask groundMask;
 
     private bool isGrounded;
-    public Vector3 velocity;
+    Vector3 velocity;
     
 
     void FixedUpdate()
@@ -22,12 +26,19 @@ public class PlayerController : MonoBehaviour
         float zMove = Input.GetAxisRaw("Vertical");
         Vector3 move = transform.right * xMove + transform.forward * zMove;
         player.Move(move * speed * Time.deltaTime);
+        stamina = Math.Clamp(stamina, 0, 100); 
         if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.W))
-        {
+        {   
             speed = sprintSpeed;
+            stamina -= (15 * Time.deltaTime);
+            if(stamina <= 0){
+                speed = baseSpeed;
+            }
         } else {
             speed = baseSpeed;
+            stamina += (10 * Time.deltaTime);
         }
+        
         isGrounded = Physics.Raycast(player.transform.position, Vector3.down, (player.height / 2) + 0.1f, groundMask);
         if (isGrounded && velocity.y < 0) {
             velocity.y = 0f;
