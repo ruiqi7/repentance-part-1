6397bb1ad3a16059a1bccb89e7a056539d46b788
JessPollard <jlpollard@student.unimelb.edu.au>
2023-08-30 11:31:29 +1000

    enemy #2 teleporting

--- Assets/Scripts/WalkThroughWalls.cs
+++ Assets/Scripts/WalkThroughWalls.cs
@@ -7,6 +7,10 @@ public class WalkThroughWalls : MonoBehaviour
     [SerializeField] private GameObject target;
     [SerializeField] private float speed;
     [SerializeField] private float distance;
+    [SerializeField] private float minX, maxX;
+    [SerializeField] private float minZ, maxZ;
+    private float startTime;
+    private Vector3 targetPosition;
     void Start()
     {
         transform.LookAt(target.transform.position);
@@ -19,6 +23,18 @@ public class WalkThroughWalls : MonoBehaviour
             Vector3 newPos = Vector3.MoveTowards(transform.position, target.transform.position, speed);
             transform.position = new Vector3(newPos.x, 0, newPos.z);
             transform.LookAt(new Vector3(target.transform.position.x, 0, target.transform.position.z));
+        } else if(Time.time - startTime >= 10) {
+            transform.position = GetRandomTarget();
+            startTime = Time.time;
+            targetPosition = GetRandomTarget();
+        } else {
+            Vector3 newPos = Vector3.MoveTowards(transform.position, targetPosition, speed);
+            transform.position = new Vector3(newPos.x, 0, newPos.z);
+            transform.LookAt(targetPosition);
         }
+    }
+
+     private Vector3 GetRandomTarget() {
+        return new Vector3(Random.Range(minX, maxX), 0f, Random.Range(minZ,maxZ));
     }
 }
