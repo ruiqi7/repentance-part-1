27cdd105da4279feaeb7ce243d9d08a34d1681a1
vk-comp <vkhuat@student.unimelb.edu.au>
2023-09-02 22:11:27 +1000

    StartScene Note

---
+++ Assets/Scripts/InteractScripts/NoteInteract.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteInteract : InteractableInterface
+{
+   public override void interact(){
+        
+    }
+}
